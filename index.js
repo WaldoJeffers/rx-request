49395308a01fbe7bb95076f2fffd278797ae2e64
Achille <achille.urbain@gmail.com>
2016-07-30 17:47:19 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const JSONStream  = require('JSONStream');
+const request     = require('request');
+const Rx          = require('rx');
+
+function rxify(method){
+  return (url, options, schema) =>
+    Rx.Observable.create(observer =>
+      method(url, options)
+        .on('response', function onResponse(res){ // needs to be an ES5 function, otherwise `this` is undefined
+          if (res.statusCode !== 200){
+            this.emit('error', {
+              statusCode    : res.statusCode,
+              statusMessage : res.statusMessage
+            });
+          }
+        })
+        .on('error', err => observer.onError(err))  // JSONStream can not handle errors. Thus, we can not pipe to JSONStream directly if there is an error
+        .pipe(JSONStream.parse(schema))
+        .on('data', data => observer.onNext(data))
+    );
+}
+
+module.exports  = {
+  get   : rxify(request.get),
+  head  : rxify(request.head)
+};
